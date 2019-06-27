8afa0df3bf238e7e91661bb7c8c9edd1df165e6d
Sanix2802 <47119344+Sanix2802@users.noreply.github.com>
2019-06-27 22:23:50 +0300

    Add files via upload

---
+++ build/precache-manifest.4836b2b35860ed9c346ac1d820fbbe94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5830eed977ffbb86a60",
+    "url": "/gorilla2/static/js/runtime~main.e5830eed.js"
+  },
+  {
+    "revision": "b39ea82ed0c40fea5963",
+    "url": "/gorilla2/static/js/main.b39ea82e.chunk.js"
+  },
+  {
+    "revision": "8370bc4fbdedf1e70a01",
+    "url": "/gorilla2/static/js/2.8370bc4f.chunk.js"
+  },
+  {
+    "revision": "b39ea82ed0c40fea5963",
+    "url": "/gorilla2/static/css/main.403b8401.chunk.css"
+  },
+  {
+    "revision": "8370bc4fbdedf1e70a01",
+    "url": "/gorilla2/static/css/2.157b6da9.chunk.css"
+  },
+  {
+    "revision": "bb13090198c0293d32f4ead4c74f7f22",
+    "url": "/gorilla2/index.html"
+  }
+];
